e6611910727cb4bde963fe2356e04aa93b159766
Farel Fazriel M <farel12345fz@gmail.com>
2026-03-06 22:09:36 +0700

    Commit 3

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.ui.login.LoginFragment
+import com.inheal.inheal.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
